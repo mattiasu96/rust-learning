47609b76ab06754ef203b7c726223d126c9321cc
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-01-28 23:59:13 +0100

    Added first parsing of args in minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+pub fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = args.get(1).expect("Error: the query value is missing!");
+    let file_path = args.get(2).expect("Error: the file_path is missing!");
+
+    (query, file_path)
 }
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (query, file_path) = parse_args(&args);
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should be able to read file");
+
+    println!("With text:\n {contents}");
+}
+
