e9723c20c54516e5220fd84f4caa31ae55d332d1
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-01-30 22:39:36 +0100

    Added error management to function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,22 +1,41 @@
 use std::env;
 use std::fs;
 
-pub fn parse_args(args: &[String]) -> (&str, &str) {
-    let query = args.get(1).expect("Error: the query value is missing!");
-    let file_path = args.get(2).expect("Error: the file_path is missing!");
+struct Config {
+    query: String,
+    file_path: String
+}
 
-    (query, file_path)
+impl Config {
+
+    fn new(args: &[String]) -> Option<Config> {
+        if args.len() < 3 {
+            panic!("Not enough input arguments")
+        }
+        let query = args.get(1)?.clone();
+        let file_path = args.get(2)?.clone();
+
+        Some(Config{query, file_path})
+
+    }
+
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_args(&args);
+    let config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = match config {
+        Some(config) => config,
+        None => panic!("Non valid config!")
+        
+    };
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read file");
+    println!("Searching for {0}", config.query);
+    println!("In file {0}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
 
     println!("With text:\n {contents}");
 }
